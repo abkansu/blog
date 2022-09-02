97aea15a1173ca31c56e7bc105e26613127dc349
unknown <abkansu@gmail.com>
2022-09-02 23:23:46 +0300

    added to resources

--- frontend/src/components/Resource.js
+++ frontend/src/components/Resource.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import image from "../images/pdf.png";
 
 const Resource = (props) => {
   return (
@@ -10,35 +11,53 @@ const Resource = (props) => {
         width: "20%",
         height: "250px",
         float: "right",
-        backgroundColor: "#15510F",
+        backgroundColor: "#6FA960",
         borderRadius: "50px",
+        position: "relative",
       }}
     >
-      <div style={{ height: "70%" }}>damn</div>
       <div
         style={{
-          marginLeft: "10%",
-          width: "70%",
-          height: "10%",
-          alignSelf: "center",
-          overflow: "hidden",
+          width: "20%",
+          height: "20%",
+          position: "absolute",
+          top: "40%",
+          left: "8%",
+          right: "0",
+          margin: "auto",
         }}
       >
-        <i
-          className="material-icons"
-          style={{ color: "red", width: "10%", float: "left" }}
-        >
+        {/* <i className="material-symbols-outlined" style={{ color: "black" }}>
           picture_as_pdf
+        </i> */}
+        <i className="material-symbols-outlined" style={{ color: "black" }}>
+          file_present
         </i>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "30%",
+          overflow: "hidden",
+          position: "absolute",
+          bottom: "15%",
+          right: "0",
+          left: "0",
+          margin: "auto",
+        }}
+      >
         <p
           style={{
-            color: "white",
-            width: "80%",
-            float: "right",
-            margin: "0%",
+            color: "black",
+            width: "70%",
+            marginLeft: "15%",
+            fontFamily: "Oxygen, sans-serif",
+            textAlign: "center",
           }}
         >
-          Pdf file name with a pdf file name
+          Pdf file name with a pdf file name Pdf file name with a pdf file name
+          Pdf file name with a pdf file name Pdf file name with a pdf file name
+          Pdf file name with a pdf file name Pdf file name with a pdf file name
         </p>
       </div>
     </motion.div>
@@ -46,3 +65,12 @@ const Resource = (props) => {
 };
 
 export default Resource;
+
+{
+  /* <i
+          className="material-icons"
+          style={{ color: "red", width: "100%", float: "left", height: "80px" }}
+        >
+          picture_as_pdf
+        </i> #3F4A3C */
+}
